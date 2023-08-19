890a5ea12530c9e23372c6be750334002ee79211
AndersonGuassu <andersonguassu@hotmail.com>
2023-08-19 10:51:12 -0300

    criando componentes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
-  <main class="colums is-gapless is-multiline">
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <barra_lateral />
+    </div>
+    <div class="column is-three-quarter">
+      <myForm />
+    </div>
+
 
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-
+import barra_lateral from './components/barra_lateral.vue';
+import myForm from './components/myForm.vue';
 
 export default defineComponent({
   name: 'App',
-  
+  components: {
+    barra_lateral,
+    myForm
+  }
 });
 </script>
 
---
+++ src/components/myForm.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa vocẽ deseja iniciar?">
+            </div>
+            <div class="column">
+                <div class="is-flex is_align-items-center is-justify-content-space-between">
+                    <cronometroView :tempoEmSegundos="tempoEmSegundos"/>
+                    <button class="button" @click="iniciar">
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>play</span>
+                    </button>
+                    <button class="button" @click="finalizar">
+                        <span class="icon">
+                            <i class="fas fa-stop"></i>
+                        </span>
+                        <span>stop</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import cronometroView from './cronometroView.vue';
+
+export default defineComponent({
+    name: 'myForm',
+    components:{
+        cronometroView,
+    },
+    data(){
+        return{
+            tempoEmSegundos: 0,
+            cronometro: 0,
+        }
+    },
+    
+    methods: {
+        iniciar (){
+            this.cronometro = setInterval(() => {
+                this.tempoEmSegundos += 1
+            }, 1000);
+            console.log('iniciando');
+        },
+        finalizar (){
+            console.log('finalizando');
+            clearInterval(this.cronometro)
+        }
+    }
+});
+</script>
+
+<script>
+
+</script>
